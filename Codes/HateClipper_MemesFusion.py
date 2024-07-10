54982652a84969cdc0840528e2fe3bfe9ebb6968
gak97 <girishakoushik@gmail.com>
2024-07-10 18:06:55 +0100

    updated code for higher accuracy and auc with higher threshold (0.7)

--- Codes/HateClipper_MemesFusion.py
+++ Codes/HateClipper_MemesFusion.py
@@ -11,9 +11,10 @@ from transformers import CLIPProcessor, CLIPModel
 from datasets import load_dataset
 import numpy as np
 import wandb
-from sklearn.metrics import accuracy_score, roc_auc_score, f1_score, precision_score, recall_score
+from sklearn.metrics import accuracy_score, roc_auc_score, f1_score, precision_score, recall_score, precision_recall_curve, roc_curve
 from torchvision import transforms
 from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.nn import BCEWithLogitsLoss
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -27,7 +28,9 @@ class MemeDataset(Dataset):
         self.images = {}
         for row in tqdm(self.data, desc="Loading images"):
             image_id = row['img']
-            image_path = f'/backup/hatemm/Hateful_Memes_Extended/{image_id}'
+            # image_id = row['img'].replace('img/', 'img_masked/')
+            # image_id = row['img'].replace('img/', 'img_inpainted/')
+            image_path = f'/backup/girish_datasets/Hateful_Memes_Extended/{image_id}'
             if os.path.exists(image_path):
                 image = Image.open(image_path).convert('RGB')
                 self.images[image_id] = image
@@ -66,14 +69,14 @@ class BilinearPooling(nn.Module):
         return output
 
 class CLIPClassifier(nn.Module):
-    def __init__(self, model_name="openai/clip-vit-large-patch14", dropout_rate=0.2, projection_dim=768, fusion_type='cross'):   # openai/clip-vit-base-patch32
+    def __init__(self, model_name="openai/clip-vit-large-patch14", dropout_rate=0.1, projection_dim=512, fusion_type='cross'):
         super(CLIPClassifier, self).__init__()
         self.clip_model = CLIPModel.from_pretrained(model_name)
         for param in self.clip_model.parameters():
             param.requires_grad = False
 
-        self.text_projection = nn.Linear(768, projection_dim)
-        self.image_projection = nn.Linear(768, projection_dim)
+        self.text_projection = nn.Linear(self.clip_model.config.text_config.hidden_size, projection_dim)
+        self.image_projection = nn.Linear(self.clip_model.config.vision_config.hidden_size, projection_dim)
         self.dropout = nn.Dropout(dropout_rate)
         self.fusion_type = fusion_type
 
@@ -88,9 +91,12 @@ class CLIPClassifier(nn.Module):
         self.output = nn.Linear(projection_dim, 1)
 
     def forward(self, input_ids, pixel_values, attention_mask=None):
-        outputs = self.clip_model(input_ids=input_ids, pixel_values=pixel_values, attention_mask=attention_mask)
-        text_features = self.text_projection(outputs.text_embeds)
-        image_features = self.image_projection(outputs.image_embeds)
+        # outputs = self.clip_model(input_ids=input_ids, pixel_values=pixel_values, attention_mask=attention_mask)
+        text_features = self.clip_model.text_model(input_ids=input_ids, attention_mask=attention_mask).pooler_output
+        image_features = self.clip_model.vision_model(pixel_values=pixel_values).pooler_output
+
+        text_features = self.text_projection(text_features)
+        image_features = self.image_projection(image_features)
         
         text_features = F.normalize(text_features, p=2, dim=1)
         image_features = F.normalize(image_features, p=2, dim=1)
@@ -101,44 +107,44 @@ class CLIPClassifier(nn.Module):
             combined_features = torch.cat((text_features, image_features), dim=1)
         elif self.fusion_type == 'cross':
             combined_features = self.bilinear_pooling(text_features, image_features)
-        elif self.fusion_type == 'align_shuffle':
-            combined_features = text_features * image_features
-            idx = torch.randperm(combined_features.size(0))
-            combined_features = combined_features[idx]
+        # elif self.fusion_type == 'align_shuffle':
+        #     combined_features = text_features * image_features
+        #     idx = torch.randperm(combined_features.size(0))
+        #     combined_features = combined_features[idx]
 
         combined_features = self.dropout(combined_features)
         pre_output = self.pre_output(combined_features)
         pre_output = self.dropout(pre_output)
         logits = self.output(pre_output).squeeze(1)
 
-        return logits, text_features, image_features
+        # return logits, text_features, image_features
+        return logits
 
-    def calculate_loss(self, logits, labels, text_features, image_features):
-        main_loss = F.binary_cross_entropy_with_logits(logits, labels)
-        text_loss = F.mse_loss(text_features, labels.unsqueeze(1).expand_as(text_features))
-        image_loss = F.mse_loss(image_features, labels.unsqueeze(1).expand_as(image_features))
-        return main_loss + 0.5 * text_loss + 0.5 * image_loss
+    def calculate_loss(self, logits, labels):
+        # main_loss = F.binary_cross_entropy_with_logits(logits, labels)
+        # text_loss = F.mse_loss(text_features, labels.unsqueeze(1).expand_as(text_features))
+        # image_loss = F.mse_loss(image_features, labels.unsqueeze(1).expand_as(image_features))
+        # return main_loss + 0.5 * text_loss + 0.5 * image_loss
+        return F.binary_cross_entropy_with_logits(logits, labels)
 
-    # def calculate_loss(self, logits, labels):
-    #     return F.binary_cross_entropy_with_logits(logits, labels)
-
-    def calculate_metrics(self, logits, labels):
+    def calculate_metrics(self, logits, labels, threshold=0.7):
         preds = torch.sigmoid(logits)  # Convert to probabilities
-        preds_binary = (preds >= 0.5).long()  # Convert to binary predictions
+        preds_binary = (preds >= threshold).long()  # Convert to binary predictions
         
         accuracy = accuracy_score(labels.cpu(), preds_binary.cpu())
         precision = precision_score(labels.cpu(), preds_binary.cpu(), zero_division=0)
         recall = recall_score(labels.cpu(), preds_binary.cpu(), zero_division=0)
         f1 = f1_score(labels.cpu(), preds_binary.cpu(), zero_division=0)
-        auc = roc_auc_score(labels.cpu(), preds.cpu()) if len(set(labels.cpu().numpy())) > 1 else float('nan') # AUC is not defined when there is only one class
+        auc = roc_auc_score(labels.cpu(), preds.cpu()) if len(set(labels.cpu().numpy())) > 1 else float('nan')
         
         return accuracy, precision, recall, f1, auc
 
 def collate_fn(batch):
     batch = [item for item in batch if item is not None]  # Filter out None samples
     if not batch:
-        return torch.tensor([]), torch.tensor([])  # Return empty tensors if batch is empty
-    
+        # return torch.tensor([]), torch.tensor([])  # Return empty tensors if batch is empty
+        return None
+
     input_ids = [item[0]['input_ids'] for item in batch]
     pixel_values = [item[0]['pixel_values'] for item in batch]
     labels = torch.stack([item[1] for item in batch])
@@ -156,15 +162,25 @@ def collate_fn(batch):
     # return {'input_ids': input_ids, 'pixel_values': pixel_values}, labels
     return {'input_ids': input_ids_padded, 'pixel_values': pixel_values, 'attention_mask': attention_mask_padded}, labels
 
+def find_optimal_threshold(y_true, y_pred):
+    # precisions, recalls, thresholds = precision_recall_curve(y_true, y_pred)
+    # f1_scores = 2 * (precisions * recalls) / (precisions + recalls)
+    # optimal_threshold = thresholds[np.argmax(f1_scores)]
+    fpr, tpr, thresholds = roc_curve(y_true, y_pred)
+    gm = np.sqrt(tpr * (1-fpr))
+    optimal_threshold = thresholds[np.argmax(gm)]
+    print(f"Optimal Threshold: {optimal_threshold}")
+    return optimal_threshold
+
 # Load dataset from Hugging Face
 # dataset = load_dataset('limjiayi/hateful_memes_expanded')
 # train_data = dataset['train']
 # val_data = dataset['validation']
 # test_data = dataset['test']
 
-train_data = "/backup/hatemm/Hateful_Memes_Extended/train.jsonl"
-val_data = ["/backup/hatemm/Hateful_Memes_Extended/dev_seen.jsonl", "/backup/hatemm/Hateful_Memes_Extended/test_seen.jsonl"]
-test_data = ["/backup/hatemm/Hateful_Memes_Extended/dev_unseen.jsonl", "/backup/hatemm/Hateful_Memes_Extended/test_unseen.jsonl"]
+train_data = "/backup/girish_datasets/Hateful_Memes_Extended/train.jsonl"
+val_data = ["/backup/girish_datasets/Hateful_Memes_Extended/dev_seen.jsonl", "/backup/girish_datasets/Hateful_Memes_Extended/test_seen.jsonl"]
+test_data = ["/backup/girish_datasets/Hateful_Memes_Extended/dev_unseen.jsonl", "/backup/girish_datasets/Hateful_Memes_Extended/test_unseen.jsonl"]
 
 # Prepare dataset and dataloader
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-large-patch14")  
@@ -181,8 +197,29 @@ train_dataset = MemeDataset(train_data, processor)
 val_datasets = [MemeDataset(data, processor) for data in val_data]
 test_datasets = [MemeDataset(data, processor) for data in test_data]
 
+# Print class distribution statistics
+# print("Class Distribution:")
+# print("Training Dataset:")
+train_labels = [row['label'] for row in train_dataset.data]
+# print(f"Class 0: {train_labels.count(0)} samples")
+# print(f"Class 1: {train_labels.count(1)} samples")
+
+# print("\nValidation Datasets:")
+# for i, val_dataset in enumerate(val_datasets):
+#     print(f"Validation Dataset {i+1}:")
+#     val_labels = [row['label'] for row in val_dataset.data]
+#     print(f"Class 0: {val_labels.count(0)} samples")
+#     print(f"Class 1: {val_labels.count(1)} samples")
+
+# print("\nTest Datasets:")
+# for i, test_dataset in enumerate(test_datasets):
+#     print(f"Test Dataset {i+1}:")
+#     test_labels = [row['label'] for row in test_dataset.data]
+#     print(f"Class 0: {test_labels.count(0)} samples")
+#     print(f"Class 1: {test_labels.count(1)} samples")
+
 batch_size = 64
-learning_rate = 1e-4
+learning_rate = 2e-5
 
 train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
 # val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_fn)
@@ -199,10 +236,25 @@ if torch.cuda.device_count() > 1:
     print("Using", torch.cuda.device_count(), "GPUs!")
     model = nn.DataParallel(model)
 
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+# optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+optimizer = optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=0.01)
+scheduler = ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=5, verbose=True)
+
+# Calculate the class weights
+class_counts = [train_labels.count(0), train_labels.count(1)]
+total_counts = sum(class_counts)
+weights = [total_counts / class_counts[i] for i in range(len(class_counts))]
+pos_weight = torch.tensor([weights[1] / weights[0]], device=device)
+
+# Use weighted loss
+criterion = BCEWithLogitsLoss(pos_weight=pos_weight)
 
 # Training loop
 num_epochs = 10  # Set the number of epochs
+best_f1 = 0
+best_val_auc = 0
+patience = 5
+epochs_no_improve = 0
 
 # wandb.init(
 #     project="hate-memes-classification",
@@ -219,19 +271,26 @@ for epoch in tqdm(range(num_epochs)):
     model.train()
     total_loss = 0.0
     for inputs, labels in train_dataloader:
+        if inputs is None:
+            continue
         input_ids = inputs['input_ids'].to(device)
         pixel_values = inputs['pixel_values'].to(device)
         attention_mask = inputs.get('attention_mask', None).to(device) if 'attention_mask' in inputs else None
         labels = labels.to(device)
         
         optimizer.zero_grad()
-        logits, text_features, image_features = model(input_ids, pixel_values, attention_mask)
-        loss = model.module.calculate_loss(logits, labels, text_features, image_features)
+        logits = model(input_ids, pixel_values, attention_mask)
+        # loss = model.module.calculate_loss(logits, labels)
+        loss = criterion(logits, labels.float())
         loss.backward()
         optimizer.step()
         
         total_loss += loss.item()
     
+    # Debugging output
+    # print(f"Sample logits: {logits[:5].detach().cpu().numpy()}")
+    # print(f"Sample labels: {labels[:5].cpu().numpy()}")
+
     print(f"Epoch {epoch+1}/{num_epochs}, Train Loss: {total_loss/len(train_dataloader)}")
     # accuracy, precision, recall, f1, auc = model.module.calculate_metrics(combined_logits, labels)
     # wandb.log({"Train Loss": total_loss/len(train_dataloader)})
@@ -244,22 +303,54 @@ for epoch in tqdm(range(num_epochs)):
     for val_dataloader in val_dataloaders:
         with torch.no_grad():
             for inputs, labels in val_dataloader:
+                if inputs is None:
+                    continue
                 input_ids = inputs['input_ids'].to(device)
                 pixel_values = inputs['pixel_values'].to(device)
-            labels = labels.to(device)
+                attention_mask = inputs.get('attention_mask', None).to(device) if 'attention_mask' in inputs else None
+                labels = labels.to(device)
 
-            logits, text_features, image_features = model(input_ids, pixel_values)
-            loss = model.module.calculate_loss(logits, labels, text_features, image_features)
-            all_preds.extend(torch.sigmoid(logits).cpu().numpy())
-            all_labels.extend(labels.cpu().numpy())
-            val_loss += loss.item()
+                logits = model(input_ids, pixel_values, attention_mask)
+                # loss = model.module.calculate_loss(logits, labels)
+                loss = criterion(logits, labels.float())
+                all_preds.extend(torch.sigmoid(logits).cpu().numpy())
+                all_labels.extend(labels.cpu().numpy())
+                val_loss += loss.item()
     
+    # optimal_threshold = find_optimal_threshold(np.array(all_labels), np.array(all_preds))
     accuracy, precision, recall, f1, auc = model.module.calculate_metrics(torch.tensor(all_preds), torch.tensor(all_labels))
-    print(f"Validation Loss: {val_loss}, Validation Accuracy: {accuracy}, Precision: {precision}, Recall: {recall}, F1 Score: {f1}, AUC: {auc}")
+    print(f"Validation Loss: {val_loss/len(val_dataloaders)}, Validation Accuracy: {accuracy}, Precision: {precision}, Recall: {recall}, F1 Score: {f1}, AUC: {auc}")
+    # print(f"Sample predictions: {all_preds[:5]}")
+    # print(f"Sample labels: {all_labels[:5]}")
     # wandb.log({"Validation Loss": val_loss, "Validation Accuracy": accuracy, "Validation Precision": precision, 
     #            "Validation Recall": recall, "Validation F1": f1, "Validation ROC AUC": auc})
 
-torch.save(model.state_dict(), 'clip_classifier.pth')
+# torch.save(model.state_dict(), 'clip_classifier.pth')
+    
+    # Adjust the decision threshold based on validation performance
+    # best_threshold = 0.5  # Initialize with default
+    # best_f1 = 0
+    # for thresh in np.linspace(0.1, 0.9, 9):
+    #     _, precision, recall, f1, auc = model.module.calculate_metrics(torch.tensor(all_preds), torch.tensor(all_labels), threshold=thresh)
+    #     if f1 > best_f1:
+    #         best_f1 = f1
+    #         best_threshold = thresh
+
+    # print(f"Best Threshold: {best_threshold} with F1 Score: {best_f1}")
+
+    scheduler.step(auc)
+    if auc > best_val_auc:
+        best_val_auc = auc
+        torch.save(model.state_dict(), 'best_model.pth')
+        epochs_no_improve = 0
+    else:
+        epochs_no_improve += 1
+        if epochs_no_improve == patience:
+            print("Early stopping")
+            break
+
+# Load the best model for testing
+model.load_state_dict(torch.load('best_model.pth'))
 
 # Test Evaluation
 model.eval()
@@ -270,12 +361,14 @@ for test_dataloader in test_dataloaders:
         for inputs, labels in test_dataloader:
             input_ids = inputs['input_ids'].to(device)
             pixel_values = inputs['pixel_values'].to(device)
+            attention_mask = inputs.get('attention_mask', None).to(device) if 'attention_mask' in inputs else None
             labels = labels.to(device)
 
-            logits, text_features, image_features = model(input_ids, pixel_values)
+            logits = model(input_ids, pixel_values, attention_mask)
             all_preds.extend(torch.sigmoid(logits).cpu().numpy())
             all_labels.extend(labels.cpu().numpy())
 
+# optimal_threshold = find_optimal_threshold(np.array(all_labels), np.array(all_preds))
 accuracy, precision, recall, f1, auc = model.module.calculate_metrics(torch.tensor(all_preds), torch.tensor(all_labels))
 print(f"Test Accuracy: {accuracy}, Precision: {precision}, Recall: {recall}, F1 Score: {f1}, AUC: {auc}")
 # wandb.log({"Test Accuracy": accuracy, "Test Precision": precision, "Test Recall": recall, 
